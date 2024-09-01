fe940a4ca54ceb785c2f7521a67be9c4d0ae6a2c
Rahil <prajapatirahil13@gmail.com>
2024-09-01 10:52:11 +0530

    db_error push

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import express from "express";
+import 'dotenv/config';
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
-
-const port = 3000;
-
+const port = process.env.PORT;
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let data = [];
 let nextId = 1;
 
 //#TODO: Adding new item
 app.post("/product", (req, res) => {
+  //#TODO:  logger.info("Post request is made to add new product");
   const { name, price } = req.body;
   const newItem = { id: nextId++, name, price };
   data.push(newItem);
@@ -61,7 +81,7 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.send("Hello there !!");
 });
-
+ 
 app.get("/login", (req, res) => {
   res.send("login here !!");
 });
